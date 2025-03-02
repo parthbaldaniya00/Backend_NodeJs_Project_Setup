ea905fb47196cc49619102b9db7236d620ec1485
Parth Baldaniya <parthbaldaniya9090@gmail.com>
2025-03-02 12:12:08 +0530

    Add Logger

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import app from './app';
 import { Config } from './config';
+import logger from './config/logger';
 
 const startServer = () => {
     const PORT = Config.PORT;
     try {
         app.listen(PORT, () => {
-            console.log(`Listening on port ${PORT}`);
+            logger.info('Server listening on port :', { port: PORT });
         });
     } catch (err) {
         console.error(err);
+        process.exit(1);
     }
 };
 
