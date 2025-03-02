8c4c9c0b008510a7595c2cb42e7c8e5ab168d5f3
Parth Baldaniya <parthbaldaniya9090@gmail.com>
2025-03-02 16:46:38 +0530

    Add Error Handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,29 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import logger from './config/logger';
+import { HttpError } from 'http-errors';
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     res.send('Welcome Project Setup');
+});
+
+// Global error handler.
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message);
+    const stutusCode = err.statusCode || 500;
+
+    res.status(stutusCode).json({
+        errors: [
+            {
+                type: err.name,
+                msg: err.message,
+                path: '',
+                location: '',
+            },
+        ],
+    });
 });
 
 export default app;
